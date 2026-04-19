7497bae83fc676c345c8d1a312e7bf0d5a5d8500
Orlando <ema.lap@outlook.com>
2026-04-18 18:05:13 -0600

    addProblem1

---
+++ MergeSortedArray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+    if (n == 0)
+      return;
+    int z = m - 1;
+    int j = n - 1;
+    int i = n + m - 1;
+    while (z >= 0 && j >= 0) {
+      if (nums2[j] <= nums1[z]) {
+        nums1[i] = nums1[z];
+        z--;
+      } else {
+        nums1[i] = nums2[j];
+        j--;
+      }
+      i--;
+    }
+    if (z < 0) {
+      while (i >= 0) {
+        nums1[i] = nums2[j];
+        j--;
+        i--;
+      }
+    }
+    if (j < 0) {
+      while (i >= 0) {
+        nums1[i] = nums1[z];
+        z--;
+        i--;
+      }
+    }
+  }
+};
