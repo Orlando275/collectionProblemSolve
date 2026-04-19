e5066c5086b23e867188618f57330b2d73d63e86
Orlando <ema.lap@outlook.com>
2026-04-18 18:37:56 -0600

    AddProblem2

---
+++ RemoveElement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    if (nums.size() == 0)
+      return 0;
+
+    int j = 0;
+    int k = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != val) { // if it's same it should save, if not you ignore
+        nums[j] = nums[i];
+        j++;
+        k++;
+      }
+    }
+    return k;
+  }
+};
