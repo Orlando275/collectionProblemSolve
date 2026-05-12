88f1654851658aced8c9878aa01a6c85fa3c7f33
Orlando <ema.lap@outlook.com>
2026-05-12 08:56:50 -0600

    LOLW opVersion

---
+++ LengthofLastWord.cpp
@@ -0,0 +1,17 @@
+
+class Solution { // O(n) time O(1) space
+public:
+  int lengthOfLastWord(string s) {
+    int length = 0;
+    for (int i = s.length() - 1; i >= 0; i--) {
+
+      if (s[i] != ' ') {
+        length++;
+
+      } else if (length != 0) {
+        return length;
+      }
+    }
+    return length;
+  }
+};
