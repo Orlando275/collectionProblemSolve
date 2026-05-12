e19ed30dadbffbcf75cc15549bca88e579bb1ff8
Orlando <ema.lap@outlook.com>
2026-05-12 08:10:10 -0600

    BTTB brute force

---
+++ bestTimeToBuys.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    int minprofit = prices[0];
+    int mxprofit = 0;
+    for (int i = 1; i < prices.size(); i++) {
+      for (int j = i + 1; j < prices.size(); j++) {
+        mxprofit = max(mxprofit, prices[j] - prices[i]);
+      }
+    }
+    return mxprofit;
+  }
+};
